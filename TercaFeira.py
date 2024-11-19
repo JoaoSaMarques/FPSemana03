d25a44a7b46232cec3501af69ab16a18bd067087
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-19 14:37:38 +0000

    print inverted phrase

--- TercaFeira.py
+++ TercaFeira.py
@@ -11,4 +11,8 @@ def Invert(frase):
     while stack:
         frase_invertida.append(stack.pop()) 
     
-    return " ".join(frase_invertida)
+    return ' '.join(frase_invertida)
+
+frase = "Esta é uma frase de exemplo"
+frase_invertida = Invert(frase)
+print(frase_invertida)  # Saída: "exemplo de frase uma é Esta"
