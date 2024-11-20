4463a11344a6d50f692b307d97cad73c7241d959
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-20 15:05:57 +0000

    add random

--- Semana3_2.py
+++ Semana3_2.py
@@ -1,8 +1,22 @@
 import collections
-import mylibrary
+from Bibliotecas import mylibrary
+import random
 
 
 queue = collections.deque()
 d = collections.OrderedDict()
 
-banana = mylibrary.sayHello()
+Banana = "AHAHAHAHAHAHA"
+
+mylibrary.sayHello(Banana)
+
+value = random.random()
+
+choices = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"]
+
+values = random.choice(choices)
+print(values)
+
+for i in range(0, 10):
+    random.shuffle(choices)
+    print(choices)
